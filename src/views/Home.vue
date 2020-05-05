c17faf736ba45bd3907b613142fd2194f271bdb9
Mohammad Raska <iban.ninja@gmail.com>
2020-05-06 04:10:30 +0700

    add item, insta, partner and footer component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <HeaderShayna/>
     <HeroShayna/>
-    
+    <ItemShayna/>
+    <InstaShayna/>
+    <PartnerShayna/>
+    <FooterShayna/>
   </div>
 </template>
 
@@ -11,13 +14,21 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderShayna from '@/components/HeaderShayna.vue'
 import HeroShayna from '@/components/HeroShayna.vue'
+import ItemShayna from '@/components/ItemShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
 
 
 export default {
   name: 'Home',
   components: {
     HeaderShayna,
-    HeroShayna 
+    HeroShayna,
+    ItemShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
   }
 }
 </script>
